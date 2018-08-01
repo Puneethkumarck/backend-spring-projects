3cb35129455fc4961763b43bad48d42d1b78bd18
PuneethKumarCk <punith.564@gmail.com>
2018-08-01 22:58:56 +0530

    rest api versioning changes

---
+++ rest_api_versioning/src/main/groovy/com/home/work/rest_api_versioning/RestApiVersioningApplication.groovy
@@ -0,0 +1,12 @@
+package com.home.work.rest_api_versioning
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class RestApiVersioningApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run RestApiVersioningApplication, args
+	}
+}
